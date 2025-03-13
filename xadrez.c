b4f3b2dffb7d9e2ac3684af55e57531540bb3105
Lucas78x <lucasbastos78x@gmail.com>
2025-03-13 19:20:04 +0000

    Adicionado a logica de movimentação do cavalo utilizando dois loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -22,6 +22,20 @@ int main() {
         printf("Esquerda\n");
         j++;
     } while (j < 8);
+    
+    // Movimentos do Cavalo: 2 casas para baixo e 1 para a esquerda (movimento em "L")
+    printf("\nMovimento do Cavalo:\n");
+
+    // Loop externo (utilizado for): move o Cavalo 1 vez para baixo (isso será repetido 2 vezes i <2)
+    for (int i = 0; i < 2; i++) {
+        // Loop interno (utilizado while): move 1 vez para a esquerda (isso será repetido até j < 1)
+        int j = 0;
+        while (j < 1) {
+            printf("Baixo\n");  // Movimentando o Cavalo para baixo
+            printf("Esquerda\n"); // Movimentando o Cavalo para a esquerda
+            j++;
+        }
+    }
 
     return 0;
 }
